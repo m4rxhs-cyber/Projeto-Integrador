7b516961416a0fe1f8dea42fa53be810c9afd61d
m4rxhs-cyber <m4rxhs3301@gmail.com>
2021-10-19 18:27:10 -0300

    Urna Versão 1

--- UrnaV1/urnaV1.py
+++ UrnaV1/urnaV1.py
@@ -26,11 +26,11 @@ votoPres4 = 0
 #lista dos candidatos
 if opcao == 1:
     print("\n+-----------CANDIDATOS------------+")
-    print("| 0 - Marina Silva                |")
-    print("| 1 - Charlie Brown Junior        |")
-    print("| 2 - Fiuk                        |")
-    print("| 3 - Elon Musk                   |")
-    print("| 4 - Delegado Da Cunha           |")
+    print("| 0 - %s                |" %(nomePres0))
+    print("| 1 - %s        |" %(nomePres1))
+    print("| 2 - %s                        |" %(nomePres2))
+    print("| 3 - %s                   |" %(nomePres3))
+    print("| 4 - %s           |" %(nomePres4))
     print("+---------------------------------+\n")
 
 #escolha do candidato
