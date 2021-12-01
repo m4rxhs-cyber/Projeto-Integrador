13f6db08e68687d01f2a20b80fb2b96f2d742910
m4rxhs-cyber <m4rxhs3301@gmail.com>
2021-12-01 20:28:37 -0300

    Atualização na versão 5

--- UrnaV5/Apuracao.py
+++ UrnaV5/Apuracao.py
@@ -1,4 +1,4 @@
-from Candidatos import candidatos
+from Candidatos import governadores, presidente
 from Anular import nulos, branco
 
 def menu():
@@ -6,18 +6,18 @@ def menu():
     print(f"""\n
 +-------------APURAÇÃO DOS VOTOS-----------------------+
 |               Votos para Governador                  |
-| Votos do(a) candidato(a) {candidatos[10]['Nome']}: {candidatos[10]['Votos']}                 |
-| Votos do(a) candidato(a) {candidatos[11]['Nome']}: {candidatos[11]['Votos']}                      |
-| Votos do(a) candidato(a) {candidatos[12]['Nome']}: {candidatos[12]['Votos']}                   |
-| Votos do(a) candidato(a) {candidatos[13]['Nome']}: {candidatos[13]['Votos']}                  |
-| Votos do(a) candidato(a) {candidatos[14]['Nome']}: {candidatos[14]['Votos']}                     |
+| Votos do(a) candidato(a) {governadores[10]['Nome']}: {governadores[10]['Votos']}                 |
+| Votos do(a) candidato(a) {governadores[11]['Nome']}: {governadores[11]['Votos']}                      |
+| Votos do(a) candidato(a) {governadores[12]['Nome']}: {governadores[12]['Votos']}                   |
+| Votos do(a) candidato(a) {governadores[13]['Nome']}: {governadores[13]['Votos']}                  |
+| Votos do(a) candidato(a) {governadores[14]['Nome']}: {governadores[14]['Votos']}                     |
 |                                                      |
 |               Votos para Presidente                  |
-| Votos do(a) candidato(a) {candidatos[0]['Nome']}: {candidatos[0]['Votos']}             |
-| Votos do(a) candidato(a) {candidatos[1]['Nome']}: {candidatos[1]['Votos']}     |
-| Votos do(a) candidato(a) {candidatos[2]['Nome']}: {candidatos[2]['Votos']}                     |          
-| Votos do(a) candidato(a) {candidatos[3]['Nome']}: {candidatos[3]['Votos']}                |
-| Votos do(a) candidato(a) {candidatos[4]['Nome']}: {candidatos[4]['Votos']}        |
+| Votos do(a) candidato(a) {presidente[0]['Nome']}: {presidente[0]['Votos']}             |
+| Votos do(a) candidato(a) {presidente[1]['Nome']}: {presidente[1]['Votos']}     |
+| Votos do(a) candidato(a) {presidente[2]['Nome']}: {presidente[2]['Votos']}                     |          
+| Votos do(a) candidato(a) {presidente[3]['Nome']}: {presidente[3]['Votos']}                |
+| Votos do(a) candidato(a) {presidente[4]['Nome']}: {presidente[4]['Votos']}        |
 |                                                      |
 |               Votos Nulos                            |
 | Votos nulos para governador: {nulos['votoNulo']['NuloGov']}                       |
